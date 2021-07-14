6ee005fcf8d2a870cd4d85c283d88cfb68353a93
Michael Doudy <michael.doudy@gmail.com>
2021-07-13 23:48:47 -0400

    Add monserrat; add basic variables; update border style

--- src/client/components/Section.js
+++ src/client/components/Section.js
@@ -2,7 +2,10 @@ import React, { forwardRef } from 'react'
 
 const Section = forwardRef(({ heading = '', children }, ref) => (
   <div className="section" ref={ref}>
-    <h2 className="section__heading">{ heading }</h2>
+    <div className="section__heading">
+      <h2>{ heading }</h2>
+      <div className="section__heading--line"/>
+    </div>
     <div className="section__contents">
       { children }
     </div>
