38066803b139f4f7684136f5b64223e1d946814b
Michael Doudy <michael.doudy@gmail.com>
2021-08-04 13:40:17 -0400

    Deployment test

--- src/client/components/ThemeBar.js
+++ src/client/components/ThemeBar.js
@@ -1,15 +1,16 @@
-import { useDispatch } from 'react-redux';
+// import { useDispatch } from 'react-redux';
 
-import { toggleTheme } from '../redux/settingsSlice';
+// import { toggleTheme } from '../redux/settingsSlice';
 import ThemeSelector from '../../../assets/icons/theme-selector.svg';
 
 const ThemeBar = () => {
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
 
   return (
     <div className="theme-bar">
       <div className="theme-bar__button">
-        <ThemeSelector onClick={() => dispatch(toggleTheme())} />
+        <ThemeSelector />
+        {/* <ThemeSelector onClick={() => dispatch(toggleTheme())} /> */}
       </div>
       <div className="theme-bar__line" />
     </div>
