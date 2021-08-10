2f9826a68f556a339b8efd3e60423481b431aab2
Michael Doudy <michael.doudy@gmail.com>
2021-08-10 09:34:52 -0400

    Add dangerouslySetInnerHTML option to contact

--- src/client/components/Contact.js
+++ src/client/components/Contact.js
@@ -1,10 +1,11 @@
 import data from '../../../src/client/lib/data.json';
 
-const { text, ctaText = 'Send Email', email } = data.contact;
+const { text, textHtml, ctaText = 'Send Email', email } = data.contact;
 
 const Contact = () => (
   <div className='contact'>
-    {text && <p className='contact__text'>{text}</p>}
+    {text && !textHtml && <p className='contact__text'>{text}</p>}
+    {textHtml && <p className='contact__text' dangerouslySetInnerHTML={{__html:textHtml}} />}
     <a
       className='contact__cta-button'
       href={`mailto:${email}`}
