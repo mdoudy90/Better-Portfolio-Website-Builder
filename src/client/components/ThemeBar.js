d3a3c6ef725870243c0bb7cddeb2a4de0274ac53
Michael Doudy <michael.doudy@gmail.com>
2021-08-10 21:13:09 -0400

    Add settings

--- src/client/components/ThemeBar.js
+++ src/client/components/ThemeBar.js
@@ -2,12 +2,16 @@ import { useDispatch } from 'react-redux';
 
 import { toggleTheme } from '../redux/settingsSlice';
 import ThemeSelector from '../../../assets/icons/theme-selector.svg';
+import data from '../../../src/client/lib/data.json';
+
+const { settings } = data;
 
 const ThemeBar = () => {
   const dispatch = useDispatch();
+  const themeBarLocation = settings.sideBarLocation === 'left' ? 'right' : 'left';
 
   return (
-    <div className="theme-bar">
+    <div className={`theme-bar theme-bar--${themeBarLocation}`}>
       <div className="theme-bar__button">
         <ThemeSelector onClick={() => dispatch(toggleTheme())} />
       </div>
