6021a1e6a028d556dd5de533f86909a5a10914be
Michael Doudy <michael.doudy@gmail.com>
2021-08-15 11:52:34 -0400

    Add custom html caps

--- src/client/components/About.js
+++ src/client/components/About.js
@@ -3,13 +3,16 @@ import Image from 'next/image';
 import FilteredImage from '../components/FilteredImage';
 import data from '../../../src/client/lib/data.json';
 
-const { content, image } = data.about;
+const { content, contentHtml, image } = data.about;
 
 const About = () => (
   <div className='about'>
     <div className='about__content'>
-      {content.map((text, i) => (
+      {content && !contentHtml && content.map((text, i) => (
         <p key={`about-part-${i}`}>{text}</p>
+      ))}
+      {contentHtml && contentHtml.map((text, i) => (
+        <p key={`about-part-${i}`} dangerouslySetInnerHTML={{__html:text}} />
       ))}
     </div>
     {image && (
