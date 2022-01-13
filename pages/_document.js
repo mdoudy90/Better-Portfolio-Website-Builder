34e4c1595552e90ec83375d493e86089f1381862
mdoudy90 <mdoudy@onesignal.com>
2022-01-13 17:06:09 -0500

    Add email notification

--- pages/_document.js
+++ pages/_document.js
@@ -17,6 +17,45 @@ class MyDocument extends Document {
         <Head>
           <link rel="icon" href={meta.favIcon} />
           <link rel="apple-touch-icon" href={meta.mobileIcon} />
+
+          <script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" async=""></script>
+          {!!process.env.NEXT_PUBLIC_ONESIGNAL_APP_ID && (
+            <script dangerouslySetInnerHTML={{
+                    __html: `
+                    window.OneSignal = window.OneSignal || [];
+                    OneSignal.push(function() {
+                      OneSignal.init({
+                        appId: "${NEXT_PUBLIC_ONESIGNAL_APP_ID}",
+                        safari_web_id: "",
+                        notifyButton: {
+                          enable: true,
+                        },
+                        promptOptions: {
+                            slidedown: {
+                              prompts: [
+                                {
+                                  type: "email",
+                                  autoPrompt: true,
+                                  text: {
+                                    "acceptButton": "Submit",
+                                    "cancelButton": "No Thanks",
+                                    "actionMessage": "Add your email and I'll reach out!",
+                                    "confirmMessage": "Thank You!",
+                                    "emailLabel": "Email Address"
+                                  },
+                                  delay: {
+                                    pageViews: 1,
+                                    timeDelay: 20
+                                  },
+                                }
+                              ]
+                            }
+                        }
+                      }
+                    });
+                `,
+                  }}>
+            </script>)}
         </Head>
         <body>
           {!!GA_TRACKING_ID && (
